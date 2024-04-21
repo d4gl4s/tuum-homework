eab8cf31279d56a3ffa7bd2544fdffc48fb760e7
d4gl4s <daglas.aitsen@gmail.com>
2024-04-21 17:04:18 +0300

    first commit

---
+++ src/main/java/com/tuum/tuumhomework/TuumHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.tuum.tuumhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuumHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuumHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuum/tuumhomework/TuumHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuum.tuumhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuumHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
